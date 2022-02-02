1758b4815a006431dfa60428a59f0da11393180c
Grant Royseth <groyseth@gmail.com>
2022-02-02 10:33:55 -0600

    added routes

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const path = require('path');
 const express = require('express');
-// const exphbs = require('express-handlebars');
-
-// const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+// const sess = {
+//   secret: 'Super secret secret',
+//   resave: false,
+//   saveUninitialized: true,
+// };
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
