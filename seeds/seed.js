ddce502496a47187a5f19a44f927c55eb7ad8274
Ceebcheng <jonathan2075@gmail.com>
2022-02-03 14:53:17 -0600

    worked on seeds user and password

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,41 +1,16 @@
-// const sequelize = require('../config/connection');
-
-// // const seedGallery = require('./galleryData');
-// // const seedPaintings = require('./paintingData');
-
-// const seedAll = async () => {
-//   await sequelize.sync({ force: true });
-
-
-
-// //   await seedGallery();
-// //   await seedPaintings();
-
-//   process.exit(0);
-// };
-
-// seedAll();
 
 const sequelize = require('../config/connection');
-const { User, Sna } = require('../models');
+const { User } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const userData = require('./userData.JSON');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
 
   process.exit(0);
 };
