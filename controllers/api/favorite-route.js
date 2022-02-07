a8e669e829ad464b9a08767bc1a757c16233b625
Grant Royseth <groyseth@gmail.com>
2022-02-07 12:45:00 -0600

    working favorite table

--- controllers/api/favorite-route.js
+++ controllers/api/favorite-route.js
@@ -16,8 +16,34 @@ router.get('/:id', async (req, res) => {
       res.status(500).json(err);
     }
   });
-  
 
+
+  router.post('/',  async (req, res) => {
+    try {
+      const newFavorite = await Favorite.create({
+        user_id: req.body.user_id,
+        sna_id: req.body.sna_id,
+        // userId: req.session.user_id,
+        
+        // ...req.body,
+        // user_id: req.session.user_id,
+        // postId: req.session.postId
+      //   include: [
+      //     {
+      //       model: User,
+      //       attributes: ['id'],
+      //     },
+      //   ],
+      // // });
+      });
+      
+      console.log(newFavorite);
+        
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 
 
