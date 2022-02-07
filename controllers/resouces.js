e356aefdc8716e3bb23e6de299150423d251ea8f
Bear Ridge <ahuss2010@live.com>
2022-02-07 14:21:07 -0600

    samesies

--- controllers/resouces.js
+++ controllers/resouces.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
-    res.render('resources', {
-       
-      });
-    } catch (err) {
+      if (!req.session.logged_in) {
+        res.redirect('/');
+    } else {  
+    res.render('resources', {});
+    } 
+  } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
